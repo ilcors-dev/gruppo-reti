b37e2ba3f91a1ee5114936eab93e01d8a27824da
Claudio Bernardini <claudio.bernardini28@gmail.com>
2021-10-10 18:05:10 +0200

    consumatore params done.

--- esercitazione_1/C/consumatore.c
+++ esercitazione_1/C/consumatore.c
@@ -2,29 +2,43 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 #define MAX_STRING_LENGTH 256
 
 // consumatore.c e' un filtro
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-	char *file_in, read_char, buf[MAX_STRING_LENGTH];
+	char *file_in, read_char, buf[MAX_STRING_LENGTH], *delete_chars;
 	int nread, fd;
-	
+
 	//controllo numero argomenti
-	if (argc != 2){ 
-		perror(" numero di argomenti sbagliato"); exit(1);
-	} 
-	
-	file_in = argv[1];
-	fd = open(file_in, O_RDONLY);
-	if (fd<0){
-		perror("P0: Impossibile aprire il file.");
-		exit(2);
+	if (argc < 2 || argc > 3)
+	{
+		perror(" numero di argomenti sbagliato");
+		exit(1);
 	}
 
-	while(nread = read(fd, &read_char, sizeof(char))) /* Fino ad EOF*/{
-		if (nread >= 0) putchar(read_char);
-		else{
+	if (argc == 3)
+	{
+		delete_chars = argv[1];
+		file_in = argv[2];
+		fd = open(file_in, O_RDONLY);
+		if (fd < 0)
+		{
+			perror("P0: Impossibile aprire il file.");
+			exit(2);
+		}
+		
+	}
+	while ((nread = read(fd, &read_char, sizeof(char)))) /* Fino ad EOF*/
+	{
+		if (nread >= 0) {
+			if ((strchr(delete_chars, read_char)) == NULL) putchar(read_char);
+		}	
+		else
+		{
 			printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
 			perror("Errore!");
 			close(fd);
