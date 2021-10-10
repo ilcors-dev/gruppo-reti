283ab678e02b78c0d42967a037c2a98e03afeb05
Claudio Bernardini <claudio.bernardini28@gmail.com>
2021-10-10 18:41:56 +0200

    updare consumatore

--- esercitazione_1/C/consumatore.c
+++ esercitazione_1/C/consumatore.c
@@ -10,40 +10,46 @@
 int main(int argc, char *argv[])
 {
 
-	char *file_in, read_char, buf[MAX_STRING_LENGTH], *delete_chars;
-	int nread, fd;
+    char *file_in, read_char, buf[MAX_STRING_LENGTH], *delete_chars;
+    int nread, fd;
 
-	//controllo numero argomenti
-	if (argc < 2 || argc > 3)
-	{
-		perror(" numero di argomenti sbagliato");
-		exit(1);
-	}
+    //controllo numero argomenti
+    if (argc < 2 || argc > 3)
+    {
+        perror(" numero di argomenti sbagliato");
+        exit(1);
+    }
 
-	if (argc == 3)
-	{
-		delete_chars = argv[1];
-		file_in = argv[2];
-		fd = open(file_in, O_RDONLY);
-		if (fd < 0)
-		{
-			perror("P0: Impossibile aprire il file.");
-			exit(2);
-		}
-		
-	}
-	while ((nread = read(fd, &read_char, sizeof(char)))) /* Fino ad EOF*/
-	{
-		if (nread >= 0) {
-			if ((strchr(delete_chars, read_char)) == NULL) putchar(read_char);
-		}	
-		else
-		{
-			printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
-			perror("Errore!");
-			close(fd);
-			exit(3);
-		}
-	}
-	close(fd);
+    delete_chars = argv[1];
+
+    if (argc == 3)
+    {
+        file_in = argv[2];
+        fd = open(file_in, O_RDONLY);
+        if (fd < 0)
+        {
+            perror("P0: Impossibile aprire il file.");
+            exit(2);
+        }
+    }
+    else
+    {
+        fd = 0; //File descriptor stdin
+    }
+    while ((nread = read(fd, &read_char, sizeof(char)))) /* Fino ad EOF*/
+    {
+        if (nread >= 0)
+        {
+            if ((strchr(delete_chars, read_char)) == NULL)
+                putchar(read_char);
+        }
+        else
+        {
+            printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
+            perror("Errore!");
+            close(fd);
+            exit(3);
+        }
+    }
+    close(fd);
 }
