515a3eeae4a6af51166114cef0c18b0878a24923
Claudio Bernardini <claudio.bernardini28@gmail.com>
2021-10-11 20:47:44 +0200

    remove unused variables and imports.

--- esercitazione_1/java/src/Produttore.java
+++ esercitazione_1/java/src/Produttore.java
@@ -2,12 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 
 public class Produttore {
     public static void main(String[] args) {
         BufferedReader in = null;
-        int res = 0;
 
         if (args.length != 1) {
             System.out.println("Utilizzo: produttore <inputFilename>");
