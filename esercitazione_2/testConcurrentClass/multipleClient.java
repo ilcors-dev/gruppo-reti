bbab0751762d480a920164b894966918b1511d4f
ilcors-dev <corsetti.luca28@gmail.com>
2021-10-24 14:00:31 +0200

    changed: multiple requests to server [benchmark class]

--- esercitazione_2/testConcurrentClass/multipleClient.java
+++ esercitazione_2/testConcurrentClass/multipleClient.java
@@ -2,18 +2,24 @@ package testConcurrentClass;
 
 public class multipleClient {
     public static void main(String[] args) {
-        String[] args1 = new String[]{"127.0.0.1", "54321", "DIRETTORIO_TOREPLACE" ,"800"};
-        String[] args2 = new String[]{"127.0.0.1", "54321", "DIRETTORIO_TOREPLACE" ,"800"};
-        String[] args3 = new String[]{"127.0.0.1", "54321", "DIRETTORIO_TOREPLACE" ,"800"};
-        String[] args4 = new String[]{"127.0.0.1", "54321", "DIRETTORIO_TOREPLACE" ,"800"};
+        String[] args1 = new String[] { "127.0.0.1", "54321", ".\\1", "1" };
+        String[] args2 = new String[] { "127.0.0.1", "54321", ".\\2", "1" };
+        String[] args3 = new String[] { "127.0.0.1", "54321", ".\\3", "1" };
+        String[] args4 = new String[] { "127.0.0.1", "54321", ".\\4", "1" };
+        String[] args5 = new String[] { "127.0.0.1", "54321", ".\\5", "1" };
+        String[] args6 = new String[] { "127.0.0.1", "54321", ".\\6", "1" };
         PutFileClientTest p1 = new PutFileClientTest(args1);
         PutFileClientTest p2 = new PutFileClientTest(args2);
         PutFileClientTest p3 = new PutFileClientTest(args3);
         PutFileClientTest p4 = new PutFileClientTest(args4);
+        PutFileClientTest p5 = new PutFileClientTest(args5);
+        PutFileClientTest p6 = new PutFileClientTest(args6);
 
         p1.start();
         p2.start();
         p3.start();
         p4.start();
+        p5.start();
+        p6.start();
     }
 }
