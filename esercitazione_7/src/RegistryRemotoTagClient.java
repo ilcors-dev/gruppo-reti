f706b7febfadf40b74f9850d12175452c95bdc38
Alangit38 <leonardo.straccali@gmail.com>
2021-11-28 16:44:01 +0100

    changed: TagClasses

--- esercitazione_7/src/RegistryRemotoTagClient.java
+++ esercitazione_7/src/RegistryRemotoTagClient.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RegistryRemotoTagClient extends RegistryRemotoClient{
-    public Remote[] cercaTag(Tag tag) throws RemoteException;
+    public String[] cercaTag(Tag tag) throws RemoteException;
 }
