b8317c01b6e001910d7fed11f763180243a17ea3
giangiu25307 <gianlucgiardina@libero.it>
2021-12-06 00:33:28 +0100

    changed: tutti i file modificati

--- esercitazione_8/src/scanClient.c
+++ esercitazione_8/src/scanClient.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <rpc/rpc.h>
-#include "count.h"
+#include "scan.h"
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     char chars, ok[128];
 
     input.directory = (char *)malloc(50);
-    input.threshold = 0;
+    input.soglia = 0;
     fileName = (char *)malloc(50);
 
     if (argc != 2)
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    clnt = clnt_create(argv[1], COUNTPROGRAM, CONTAVERS, "udp");
+    clnt = clnt_create(argv[1], SCANPROGRAM, SCANVERS, "udp");
 
     if (clnt == NULL)
     {
@@ -33,28 +33,26 @@ int main(int argc, char *argv[])
     }
 
     printf("richieste servizio fino a fine file\n");
-    printf("operazioni: CS = Conta File maggiori di, C = Conta\n");
+    printf("operazioni: SD = Scansiona File maggiori di nella Directory specificata, SF = Scansiona File\n");
 
     while (gets(ok))
     {
-        if ((strcmp(ok, "CS") != 0) && (strcmp(ok, "C") != 0))
+        if ((strcmp(ok, "SD") != 0) && (strcmp(ok, "SF") != 0))
         {
             printf("scelta non disponibile\n");
             printf("richieste servizio fino a fine file\n");
-            printf("operazioni: CS = Conta File maggiori di, C = Conta\n");
+            printf("operazioni: SD = Conta File maggiori di, SF = Conta\n");
             continue;
         }
 
-        printf("Richiesto servizio: %s\n", ok);
-
         // richiesta conteggio file nel directory remoto
-        if (strcmp(ok, "CS") == 0)
+        if (strcmp(ok, "SD") == 0)
         {
             printf("inserisci il nome directory: \n");
             gets(input.directory);
             printf("inserisci la threshold: \n");
             //controllo intero
-            while (scanf("%d", &input.threshold) != 1)
+            while (scanf("%d", &input.soglia) != 1)
             {
                 do
                 {
@@ -74,23 +72,23 @@ int main(int argc, char *argv[])
             else if (*ris == -1)
                 printf("E' avvenuto un errore lato server\n");
             else
-                printf("Ho contato %d file con dim >= %d!\n", *ris, input.threshold);
+                printf("Ho contato %d file con dim >= %d!\n", *ris, input.soglia);
         } // CS
 
         // richiesta conteggio characters nel file remoto
-        else if (strcmp(ok, "C") == 0)
+        else if (strcmp(ok, "SF") == 0)
         {
             printf("inserisci il nome del file: \n");
             gets(fileName);
-            out = conta_1(&fileName, clnt);
+            out = file_scan_1(&fileName, clnt);
 
             if (out == (int *)NULL)
                 clnt_perror(clnt, "call failed");
-            else if (out->errorCode == -1)
+            else if (out->errore == -1)
                 printf("E' avvenuto un errore lato server\n");
             else
                 printf("Ho contato %d characters, %d words e %d lines !\n",
-                       out->characters, out->words, out->lines);
+                       out->caratteri, out->parole, out->linee);
             //clean input buffer
             memset(fileName, 0, sizeof(fileName));
         } // CF
