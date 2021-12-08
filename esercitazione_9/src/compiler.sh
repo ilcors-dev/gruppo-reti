d3a0d9f9c2199eb748a0dcd67110e83f71d41c62
Alangit38 <leonardo.straccali@gmail.com>
2021-12-08 15:05:07 +0100

    readded: esercitazione_9 init file

--- esercitazione_9/src/compiler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "compiling from .x file..";
-rpcgen scan.x;
-gcc scanClient.c scan_clnt.c scan_xdr.c -g -o client 2> /dev/null;
-gcc scan_svc.c scanServer.c scan_xdr.c -g -o server;
