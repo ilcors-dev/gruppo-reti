1b14744dd791171769efb99dd70a63a13d3a4e31
Claudio Bernardini <claudio.bernardini28@gmail.com>
2021-12-14 09:26:42 +0000

    Updated: client implementation

--- esercitazione_9/src/fattorex_client.c
+++ esercitazione_9/src/fattorex_client.c
@@ -5,29 +5,29 @@
 #include "fattorex.h"
 
 #define DIM 256
+#define DIMINPUTSTRING 2
 #define N 3	//numero giudici
 
 int main (int argc, char *argv[])
 {
-	char *host; 
-  	CLIENT *gestoreTrasporto; 
+	char *host, choice[DIMINPUTSTRING];
+	int *resault;
 
-	int *ris;
-  	Classifica *classificaGiudici;
+  	CLIENT *gestoreTrasporto; //Gestore di trasporto
 
-  	char ok[DIM];
-	Voto * votazione;
+  	Classifica *classificaGiudici; //Struttura di Output
+	Voto * votazione; //Struttura di Input
 
 
   	if (argc > 3){//verifica parametri invocazione
-    		printf ("usage: %s server_host\n", argv[0]);
+    		printf ("Errore invocazione: %s [serverHost]\n", argv[0]);
     		exit (1);
   	}
   	host = (argc == 2) ? argv[1] : "localhost"; //se non specificato viene assegnato "localhost"
 
 	votazione = malloc(sizeof(Voto));
 	votazione->nomeCandidato = (char*)malloc(DIM);
-	printf("Creazione struttura dati input avvenuta con successo!\n");
+	printf("Creazione struttura dati Input avvenuta con successo!\n");
 
 	gestoreTrasporto = clnt_create (host, VOTAFATTOREX, VOTAFATTOREXVERS, "udp");
 	if (gestoreTrasporto == NULL){
@@ -37,16 +37,14 @@ int main (int argc, char *argv[])
 	printf("Creazione del gestore di trasporto avvenuta con successo!\n");
 
 
-	//Interazione con l'utente
 	printf("Inserire una:\n1\tClassifica Giudici\n2\tEsprimi voto\n^D\tper terminare: ");
 
-	while (scanf("%s",ok)==1){
-		if( strcmp(ok,"1")==0 ){
+	while (scanf("%s",choice)==1){
+		if( strcmp(choice,"1")==0 ){
 			void *v;
 			// Invocazione remota
 			classificaGiudici = classifica_giudici_1(v, gestoreTrasporto);
 			
-
 			//Controllo del risultato
 			if(classificaGiudici == NULL){
 				//Errore di RPC
@@ -54,7 +52,7 @@ int main (int argc, char *argv[])
 				exit(1);
 			}
 			printf("Invocazione classifica_giudici avvenuta con successo!\n");
-			printf("Classifica ordinata giudici:\n");
+			printf("Stampa esito:\n");
 
 			for(int i=0; i<N; i++) {
 				if(classificaGiudici->classificaGiudici[i].punteggioTot > 0) {
@@ -64,28 +62,28 @@ int main (int argc, char *argv[])
 
 		} // if 1
 
-		else if( strcmp(ok,"2")==0 ){
+		else if( strcmp(choice,"2")==0 ){
 			printf("\nInserisci il nome del candidato: \n");
 			scanf("%s", votazione->nomeCandidato);
-			getchar();
+			while (getchar() != '\n'); //pulisco buffer da eventuale garbage prima di lettura carattere
 
 			printf("\nInserisci tipo di operazione (A (addiziona) oppure (S) sottrai ): \n");
 			scanf("%c", &votazione->tipoOp);
 
 			// Invocazione remota
-			ris = esprimi_voto_1(votazione, gestoreTrasporto);
+			resault = esprimi_voto_1(votazione, gestoreTrasporto);
 
 			//Controllo del risultato
-			if(ris == NULL){
+			if(resault == NULL){
 				//Errore di RPC
 				clnt_perror(gestoreTrasporto, host);
 				exit(1);
 			}
-
-			if(*ris < 0)
+			printf("Invocazione esprimi_voto avvenuta con successo!\n");
+			if(*resault < 0)
 				//Eventuale errore di logica del programma
 				printf("Problemi nell'attribuzione del voto, nome non trovato\n");
-			else if(*ris == 0)
+			else if(*resault == 0)
 				//Tutto ok
 				printf("Votazione effettuata con successo\n");
 		} // if 2
