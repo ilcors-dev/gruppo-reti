ccff343a7620992d336706664dab5d7104c16f37
Claudio Bernardini <claudio.bernardini28@gmail.com>
2021-12-14 13:11:29 +0000

    Updated: msg prompt client.

--- esercitazione_9/src/fattorex_client.c
+++ esercitazione_9/src/fattorex_client.c
@@ -40,12 +40,9 @@ int main (int argc, char *argv[])
 	while (scanf("%s",choice)==1){
 		if( strcmp(choice,"1")==0 ){
 			void *v;
-			// Invocazione remota
 			classificaGiudici = classifica_giudici_1(v, gestoreTrasporto);
 			
-			//Controllo del risultato
 			if(classificaGiudici == NULL){
-				//Errore di RPC
 				clnt_perror(gestoreTrasporto, host);
 				exit(1);
 			}
@@ -54,11 +51,11 @@ int main (int argc, char *argv[])
 
 			for(int i=0; i<NUMGIUDICI; i++) {
 				if(classificaGiudici->classificaGiudici[i].punteggioTot > 0) {
-						printf("%s con %d voti\n", classificaGiudici->classificaGiudici[i].nomeGiudice, classificaGiudici->classificaGiudici[i].punteggioTot);
+						printf("Giudice %s - %d VOTI\n", classificaGiudici->classificaGiudici[i].nomeGiudice, classificaGiudici->classificaGiudici[i].punteggioTot);
 				}
 			}
 
-		} // if 1
+		} 
 
 		else if( strcmp(choice,"2")==0 ){
 			printf("\nInserisci il nome del candidato: \n");
@@ -68,30 +65,24 @@ int main (int argc, char *argv[])
 			printf("\nInserisci tipo di operazione (A (addiziona) oppure (S) sottrai ): \n");
 			scanf("%c", &votazione->tipoOp);
 
-			// Invocazione remota
 			resault = esprimi_voto_1(votazione, gestoreTrasporto);
 
-			//Controllo del risultato
 			if(resault == NULL){
-				//Errore di RPC
 				clnt_perror(gestoreTrasporto, host);
 				exit(1);
 			}
 			printf("Invocazione esprimi_voto avvenuta con successo!\n");
 			if(*resault < 0)
-				//Eventuale errore di logica del programma
-				printf("Problemi nell'attribuzione del voto, nome non trovato\n");
+				printf("Assegnazione del voto non riuscito: inserire nome candidato valido e operazione [A|S]\n");
 			else if(*resault == 0)
-				//Tutto ok
-				printf("Votazione effettuata con successo\n");
-		} // if 2
+				printf("Votazione registrata con successo\n");
+		} 
 		else
-			printf("Operazione richiesta non disponibile!!\n");
+			printf("Operazione richiesta non gestita dal server!\n");
 
 		printf("Inserire:\n1\tGiudice in testa\n2\tEsprimi voto\n^D\tper terminare: ");
-	} // while
+	} 
 
-	// Libero le risorse, distruggendo il gestore di trasporto
 	clnt_destroy (gestoreTrasporto);
 	exit(0);
-}//main
+}
